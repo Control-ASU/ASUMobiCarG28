7ef6ffe68be15a6785a7d65621a9d5d87faa1234
abanoub-entesar <36308887+abanoub-entesar@users.noreply.github.com>
2018-02-22 13:26:57 +0200

    Add files via upload

---
+++ Sheets/sheet1_Q1-abanoub-entesar.ino
@@ -0,0 +1,13 @@
+#define LED 13
+#define T 2000
+
+void setup() {
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED,HIGH);
+  delay(T/4);
+  digitalWrite(LED,LOW);
+  delay((3*T)/4);
+}
